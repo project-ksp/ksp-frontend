05c0261c8961ef9f1e1beda0be963e0503221b35
Alifyan <genzo@pop-os.localdomain>
2024-03-22 03:04:26 +0700

    add atur publish view, atur publish proses, atur publish berhasil with modal components

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,7 +1,14 @@
+"use client";
+
 import Link from "next/link";
-import React from "react";
+import { useState } from "react";
+import Modal from "@/components/Modal";
 
 const Dashboard = () => {
+  const [showAturPublish, setAturPublish] = useState(false);
+  const [showProsesData, setProsesData] = useState(false);
+  const [showBerhasil, setBerhasil] = useState(false);
+
   return (
     <div className="flex flex-col gap-[20px]">
       <div className="flex h-[48px] gap-[36px]">
@@ -257,7 +264,7 @@ const Dashboard = () => {
           </thead>
           <tbody>
             <tr className=" border-b border-[#DDE1E6]">
-              <td className="py-[13px]">01</td>
+              <td>01</td>
               <td>Mojokerto</td>
               <td>M.Ikhsan M</td>
               <td>999,999</td>
@@ -266,13 +273,20 @@ const Dashboard = () => {
               <td>Rp. 99,999,999</td>
               <td>99</td>
               <td>
-                <div className="bg-filled-color mx-[10px] my-[3px] text-black rounded-lg text-center">
-                  Atur
+                <div className="flex justify-center items-center h-[50px]">
+                  <button
+                    onClick={() => setAturPublish(true)}
+                    className="bg-filled-color px-[20px] py-[1px] text-black rounded-lg mx-auto my-auto"
+                  >
+                    Atur
+                  </button>
                 </div>
               </td>
               <td>
-                <div className="bg-filled-color mx-[10px] my-[3px] text-black rounded-lg text-center">
-                  Akses
+                <div className="flex justify-center items-center h-[50px]">
+                  <button className="bg-filled-color px-[20px] py-[1px] text-black rounded-lg mx-auto my-auto">
+                    Akses
+                  </button>
                 </div>
               </td>
             </tr>
@@ -499,6 +513,136 @@ const Dashboard = () => {
           </tbody>
         </table>
       </div>
+      <Modal
+        isVisible={showAturPublish}
+        onClose={() => {
+          setAturPublish(false);
+        }}
+      >
+        <h3 className="text-black font-bold text-xl">Atur Publish</h3>
+        <div className="flex gap-3">
+          <div className="flex-col w-1/2">
+            <label htmlFor="cabang">Cabang</label>
+            <input
+              id="cabang"
+              type="text"
+              className="w-full py-[10px] px-[20px] border border-[#d9d9d9] rounded-xl mt-[5px] bg-white focus:outline-none"
+              placeholder="ID Cabang"
+            />
+          </div>
+          <div className="flex-col w-1/2">
+            <label htmlFor="jumlahAnggota">Jumlah Anggota</label>
+            <input
+              id="jumlahAnggota"
+              type="text"
+              className="w-full py-[10px] px-[20px] border border-[#d9d9d9] rounded-xl mt-[5px] bg-white focus:outline-none"
+              placeholder="Jumlah Anggota"
+            />
+          </div>
+        </div>
+        <div className="flex gap-3">
+          <div className="flex-col w-1/2">
+            <label htmlFor="jumlahAnggotaAktif">Jumlah Anggota Aktif</label>
+            <input
+              id="jumlahAnggotaAktif"
+              type="text"
+              className="w-full py-[10px] px-[20px] border border-[#d9d9d9] rounded-xl mt-[5px] bg-white focus:outline-none"
+              placeholder="Jumlah Anggota Aktif"
+            />
+          </div>
+          <div className="flex-col w-1/2">
+            <label htmlFor="jumlahAnggotaTidakAktif">
+              Jumlah Anggota Tidak Aktif
+            </label>
+            <input
+              id="jumlahAnggotaTidakAktif"
+              type="text"
+              className="w-full py-[10px] px-[20px] border border-[#d9d9d9] rounded-xl mt-[5px] bg-white focus:outline-none"
+              placeholder="Jumlah Anggota Tidak Aktif"
+            />
+          </div>
+        </div>
+        <div className="flex flex-col w-full">
+          <label htmlFor="aturJumlahPublish">Atur Jumlah Publish</label>
+          <input
+            id="aturJumlahPublish"
+            type="text"
+            className="w-full py-[10px] px-[20px] border border-[#d9d9d9] rounded-xl mt-[5px] bg-white focus:outline-none"
+            placeholder="Masukkan Jumlah yang Sesuai"
+          />
+          <p className="text-sm text-[#525256]">
+            Maksimal sesuai jumlah anggota aktif
+          </p>
+        </div>
+        <button
+          type="button"
+          className="w-[148px] px-[20px] py-[12px] text-white bg-primary rounded-lg ml-auto"
+          onClick={(e) => {
+            setAturPublish(false);
+            setProsesData(true);
+          }}
+        >
+          Simpan
+        </button>
+      </Modal>
+      <Modal isVisible={showProsesData} onClose={() => setProsesData(false)}>
+        <h3 className="text-xl text-center font-bold text-black">
+          Proses Data?
+        </h3>
+        <p className="text-base text-black font-regular text-center mb-4">
+          Anda yakin telah mengisikan data dengan benar dan sesuai?
+        </p>
+        <button
+          type="submit"
+          className="w-[450px] px-[20px] py-[12px] text-white bg-primary rounded-lg mx-auto"
+          onClick={(e) => {
+            setProsesData(false);
+            setBerhasil(true);
+          }}
+        >
+          Proses Data
+        </button>
+      </Modal>
+      <Modal isVisible={showBerhasil} onClose={() => setBerhasil(false)}>
+        <div className="w-[98px] h-[98px] rounded-full bg-primary place-self-center relative">
+          <svg
+            width="43"
+            height="43"
+            viewBox="0 0 43 43"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className="absolute top-0 bottom-0 right-0 left-0 m-auto"
+          >
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M27.9622 8.30487C22.0269 5.93076 14.8128 7.27682 10.608 11.9082C8.54111 14.1835 7.24452 17.0533 6.90334 20.1082C6.56216 23.1632 7.1935 26.2479 8.70746 28.9232C10.2214 31.5984 12.5408 33.7279 15.3354 35.0082C18.13 36.2886 21.2573 36.6547 24.2721 36.0543C30.3567 34.8443 34.8592 29.357 36.0597 23.3543C36.2402 22.4519 37.118 21.8667 38.0204 22.0472C38.9228 22.2277 39.508 23.1055 39.3276 24.0079C37.9245 31.0233 32.6109 37.7938 24.9225 39.3228C21.2201 40.06 17.3794 39.6104 13.9473 38.038C10.5151 36.4655 7.66647 33.8502 5.80708 30.5645C3.94769 27.2789 3.1723 23.4903 3.59132 19.7384C4.01033 15.9865 5.60231 12.4625 8.14058 9.66804M8.14058 9.66804C13.3816 3.89568 22.1173 2.37758 29.1999 5.21062C30.0543 5.5524 30.4699 6.52214 30.1281 7.37659C29.7864 8.23105 28.8166 8.64665 27.9622 8.30487"
+              fill="white"
+            />
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M40.8946 7.60151C41.5645 8.23243 41.5962 9.287 40.9652 9.95696L23.2868 28.7289C22.9779 29.0569 22.5493 29.2459 22.0987 29.2526C21.6482 29.2594 21.2141 29.0834 20.8955 28.7648L14.3865 22.2558C13.7358 21.605 13.7358 20.55 14.3865 19.8993C15.0372 19.2485 16.0923 19.2485 16.743 19.8993L22.0379 25.1941L38.5391 7.67217C39.17 7.00222 40.2246 6.97058 40.8946 7.60151Z"
+              fill="white"
+            />
+          </svg>
+        </div>
+        <h4 className="text-black text-xl font-bold text-center">
+          Data Berhasil Disimpan
+        </h4>
+        <p className="text-black font-regular text-base text-center mb-3">
+          Silahkan Cek Data Anda
+        </p>
+        <button
+          type="button"
+          className="w-[450px] px-[20px] py-[12px] text-white bg-primary rounded-lg mx-auto"
+          onClick={(e) => {
+            setBerhasil(false);
+          }}
+        >
+          OK
+        </button>
+      </Modal>
     </div>
   );
 };
