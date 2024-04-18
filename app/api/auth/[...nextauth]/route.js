f254698d6cd6b7ec8a98a618125db37d84fe7ca3
Alifyan <genzo@pop-os.localdomain>
2024-04-18 22:49:33 +0700

    integration login, add protected routes for login and fix some styles in login page

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,53 @@
+import NextAuth from "next-auth/next";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { jwtDecode } from "jwt-decode";
+
+export const authOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      async authorize(credentials, req) {
+        const res = await fetch(`${process.env.API_URI}login`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            username: credentials.username,
+            password: credentials.password,
+          }),
+        });
+
+        if (res.ok) {
+          return await res.json();
+        }
+        return null;
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) token.token = user.token;
+      return token;
+    },
+    async session({ session, token }) {
+      const decoded = jwtDecode(token.token);
+      session.token = token.token;
+      session.user = {
+        id: decoded.id,
+        username: decoded.username,
+        role: decoded.role,
+        name: decoded.name,
+      };
+      return session;
+    },
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  pages: {
+    signIn: "/login",
+  },
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,8 @@
 import { Manrope } from "next/font/google";
 import "./globals.css";
 
+import AuthProvider from "@/components/providers/AuthProvider";
+
 const manrope = Manrope({ subsets: ["latin"] });
 
 export const metadata = {
@@ -11,7 +13,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${manrope.className} bg-mainBg`}>{children}</body>
+      <AuthProvider>
+        <body className={`${manrope.className} bg-mainBg`}>{children}</body>
+      </AuthProvider>
     </html>
   );
 }
