205ced2c1f9620bbbe75fb81c58a1bd169b48bdc
Alifyan <genzo@pop-os.localdomain>
2024-09-06 17:06:22 +0700

    change nextjs config for image url

--- next.config.mjs
+++ next.config.mjs
@@ -11,6 +11,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "api-testing.kspsentosamakmurjatim.id",
       },
+      {
+        protocol: "https",
+        hostname: "api.kspsentosamakmurjatim.id",
+      },
     ],
   },
 };
